2d6b12f7f74d09338f28a179306ea8dd7f5630d4
Paul Starodub <amorallex@gmail.com>
2023-06-11 21:25:53 +0300

    Updated selenium_form_chrom.py with read from cookies

--- selenium_cookies.py
+++ selenium_cookies.py
@@ -15,23 +15,23 @@ options.add_argument(
 driver = webdriver.Chrome(options=options)
 
 try:
-    driver.get(
-        url="https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com"
-    )
-    time.sleep(1)
-
-    email_input = driver.find_element(By.NAME, "email")
-    email_input.clear()
-    email_input.send_keys("")  # correct email
-    time.sleep(1)
-
-    password_input = driver.find_element(
-        By.CSS_SELECTOR, "input#current-password"
-    )
-    password_input.clear()
-    password_input.send_keys("")  # correct password
-    password_input.send_keys(Keys.ENTER)
-    time.sleep(6)
+    # driver.get(
+    #     url="https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com"
+    # )
+    # time.sleep(1)
+    #
+    # email_input = driver.find_element(By.NAME, "email")
+    # email_input.clear()
+    # email_input.send_keys("")  # correct email
+    # time.sleep(1)
+    #
+    # password_input = driver.find_element(
+    #     By.CSS_SELECTOR, "input#current-password"
+    # )
+    # password_input.clear()
+    # password_input.send_keys("")  # correct password
+    # password_input.send_keys(Keys.ENTER)
+    # time.sleep(6)
 
     # login_button = driver.find_element(
     #     By.CSS_SELECTOR, ".LoginModal_cta_bottom_box_button_login__5Fbwv"
@@ -39,7 +39,17 @@ try:
     # time.sleep(7)
 
     # cookies
-    pickle.dump(driver.get_cookies(), open("cookies", "wb"))
+    # pickle.dump(driver.get_cookies(), open("cookies", "wb"))
+
+    driver.get("https://profile.w3schools.com/")
+    time.sleep(5)
+    for cookie in pickle.load(open("cookies", "rb")):
+        driver.add_cookie(cookie)
+
+    time.sleep(5)
+    driver.refresh()
+    time.sleep(10)
+
 
 except Exception as e:
     print(e)
